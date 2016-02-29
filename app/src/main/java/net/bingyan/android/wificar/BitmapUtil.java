0a3265db5b836e27879e11974743ae7a5d389839
iamjinge <jiangjin863041@gmail.com>
2016-02-29 13:58:10 +0800

    add module

---
+++ app/src/main/java/net/bingyan/android/wificar/BitmapUtil.java
@@ -0,0 +1,12 @@
+package net.bingyan.android.wificar;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Jinge on 2016/2/29.
+ */
+public class BitmapUtil {
+    public static void decode(Bitmap bitmap) {
+        int pixle = bitmap.getPixel(0, 0);
+    }
+}
